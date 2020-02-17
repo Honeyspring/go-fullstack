25c28dae536c1b7a8ed28e3c644b122b7eebdd0d
Ogunsola oyindamola <honeyspringworld@gmail.com>
2020-02-17 11:33:04 +0100

    incoperated mongoose db

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
-//implemented CORS to make sure the front end could safely make calls to your app.
+const Thing = require('./models/thing'); //connect database schema
+
+//connect databe mongoose to server
+mongoose.connect('mongodb+srv://honeyspring:greenland@cluster0-xoilo.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+  //implemented CORS to make sure the front end could safely make calls to your app.
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-
 app.use(bodyParser.json());//to extract the JSON object from the request
 app.use('/api/stuff', (req, res, next) => {
   const stuff = [
