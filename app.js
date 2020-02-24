1ba7eec8dd2d5352b9ee042f543c0fe6f7caa373
Ogunsola oyindamola <honeyspringworld@gmail.com>
2020-02-24 21:14:46 +0100

    optimize backend structure

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const app = express();
-const Thing = require('./models/thing'); //connect table schema
+
 
 
 //connect databe mongoose to server
@@ -23,104 +22,10 @@ app.use((req, res, next) => {
   next();
 });
 app.use(bodyParser.json());//to extract the JSON object from the request
+;
 
-app.post('/api/stuff', (req, res, next) => {
-//created a new instance of your  Thing  model, passing it a JavaScript object containing all of the information it needs from the parsed request body
-  const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  //save()method returns a promise, so in our  then()  block, we send back a success response, and in our  catch()  block, 
-  //we send back an error response with the error thrown by Mongoose.
-  thing.save().then(
-    () => {
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-
-});
-//get details of individual thing through id
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({
-    _id: req.params.id
-  }).then(
-    (thing) => {
-      res.status(200).json(thing);
-    }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      });
-    }
-  );
-});
-//update data of products
-app.put('/api/stuff/:id', (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
-      res.status(201).json({
-        message: 'Thing updated successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-//delete route
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-//GET route to return all of the Things in the database
-app.use('/api/stuff', (req, res, next) => {
-  Thing.find().then(
-    (things) => {
-      res.status(200).json(things);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-
+const stuffRoutes = require('./routes/stuff'); //import route
+const userRoutes = require('./routes/user');
+app.use('/api/stuff', stuffRoutes); //register  route
+app.use('/api/user', userRoutes);
 module.exports = app;
